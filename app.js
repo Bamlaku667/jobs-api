479014a0ba4012c077e5b8af90c299da1dee8138
Bamlaku <bamlakhiruya@gmail.com>
2023-11-10 00:31:37 +0300

    authorization check for the job manipulation

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const errorHandler = require('./middlewares/errorHandler');
 const notFound = require('./middlewares/notFound');
 const authRouter = require('./routes/authRoute');
 const jobRouter = require('./routes/jobsRoute');
-
+const authenticateUser = require('./middlewares/auth');
 var app = express();
 
 // view engine setup
@@ -19,7 +19,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/api/v1/auth', authRouter);
-app.use('/api/v1/jobs', jobRouter);
+app.use('/api/v1/jobs', authenticateUser, jobRouter);
 
 app.use(notFound);
 app.use(errorHandler);
--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,22 +1,22 @@
-const jwt = require("jsonwebtoken");
 require("dotenv").config();
+const UnauthorizedError = require("../errors/unauthorized");
+const jwt = require("jsonwebtoken");
 const { TOKEN_KEY } = process.env;
-const { BadRequestError, UnauthorizedError } = require("../errors");
 
 const verifyToken = async (req, res, next) => {
   const authHeader = req.headers.authorization;
   if (!authHeader || !authHeader.startsWith("Bearer ")) {
-    throw new UnauthorizedError('No Token');
+    throw new UnauthorizedError("No Token");
   }
+
   const token = authHeader.split(" ")[1];
   try {
     const decoded = jwt.verify(token, TOKEN_KEY);
-    req.user = decoded;
-    console.log(decoded);
-  } catch (err) {
-    throw new UnauthorizedError("Unauthorized access to this route")
+    req.user = {userId: decoded.userId, name: decoded.name};
+    next();
+  } catch (error) {
+    throw new UnauthorizedError("Not authorized for this route");
   }
-  next();    
 };
 
 module.exports = verifyToken;
